75137abba420b10b56e065d754168df162e47c38
n3rada <72791564+n3rada@users.noreply.github.com>
2026-05-21 15:05:36 +0200

    Update logbook.py

--- src/toboggan/core/utils/logbook.py
+++ src/toboggan/core/utils/logbook.py
@@ -45,11 +45,11 @@ def _format_message(record):
         "TRACE": (f"<fg {trace_brown}>[*]</fg {trace_brown}>", trace_brown),
         "DEBUG": (f"<fg {debug_blue}>[•]</fg {debug_blue}>", debug_blue),
         "INFO": (f"<fg {info_white}>[i]</fg {info_white}>", info_white),
-        "SUCCESS": (f"<fg {success_green}>[]</fg {success_green}>", success_green),
+        "SUCCESS": (f"<fg {success_green}>[✓]</fg {success_green}>", success_green),
         "WARNING": (f"<fg {warning_orange}>[!]</fg {warning_orange}>", warning_orange),
-        "ERROR": (f"<fg {error_red}>[]</fg {error_red}>", error_red),
+        "ERROR": (f"<fg {error_red}>[✗]</fg {error_red}>", error_red),
         "CRITICAL": (
-            f"<fg {critical_magenta}><bold>[]</bold></fg {critical_magenta}>",
+            f"<fg {critical_magenta}><bold>[⚠]</bold></fg {critical_magenta}>",
             critical_magenta,
         ),
     }
